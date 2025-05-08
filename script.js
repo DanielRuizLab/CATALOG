1c1016b58b64aeb4f215d8ffe6d1771305320f62
DanielRuizLab <ruizperezdanielandres@gmail.com>
2025-05-08 12:09:58 -0500

    ACT RAP 08.0.5.20.25

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function handleExcelLoad() {
 
   uploadExcel.style.display = 'inline-block';
   
-  fetch('Datexce/Catálogo actualizado 25 de Abril.xlsx')
+  fetch('Datexce/Catálogo actualizado 8 mayo.xlsx')
     .then(response => {
       if (!response.ok) throw new Error('Error al cargar el archivo');
       return response.arrayBuffer();
